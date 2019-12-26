939293c2dd8c33a250b325b8c8d7ff7c30b6642e
Tommy Do <tommy.td89@gmail.com>
2019-12-26 14:37:04 -0500

    connect input data from first page to main page

--- pomodoro.py
+++ pomodoro.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 
 class Pomodoro(tk.Tk):
-
 	def __init__(self, *args, **kwargs):
 
 		tk.Tk.__init__(self, *args, **kwargs)
@@ -12,32 +11,51 @@ class Pomodoro(tk.Tk):
 		container.grid_rowconfigure(0, weight=1)
 		container.grid_columnconfigure(0, weight=1)
 
-		self.frames = {}
+		self.frames = {} # dictionary for the different pages in the app
+		self.focus_duration = tk.StringVar()
+		self.break_duration = tk.StringVar()
 
-		for F in (StartPage, MainPage):
+		for F in (StartPage, MainPage): # loop to initialize the different pages of the app
 
 			frame = F(container, self)
 
 			self.frames[F] = frame
 			frame.grid(row=0, column=0, sticky="nsew")
 
-		self.show_frame(StartPage)
+		self.show_frame(StartPage) 
 
-	def show_frame(self, cont):
-
+	def show_frame(self, cont): # function to switch frames
 		frame = self.frames[cont]
 		frame.tkraise()
 
 class StartPage(tk.Frame):
 
+
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
 
-		# focus/break labels
+		def onNext():
+			controller.focus_duration.set(focus_text.get())
+			controller.break_duration.set(break_text.get())
+			controller.show_frame(MainPage)
+
+		# labels
 		focus_label = tk.Label(self, text="Focus duration:")
 		break_label = tk.Label(self, text="Break duration:")
 		focus_label.grid(row=0, sticky='W') 
 		break_label.grid(row=1, sticky='W') 
+
+		# inputs
+		focus_text = tk.StringVar()
+		break_text = tk.StringVar()
+		focus_input = tk.Entry(self, textvariable=focus_text, width=5)
+		break_input = tk.Entry(self, textvariable=break_text, width=5)
+		focus_input.grid(row=0, column=1)
+		break_input.grid(row=1, column=1)
+
+		# button
+		next_button = tk.Button(self, text="Next", command=onNext)
+		next_button.grid(row=2, columnspan=3, sticky='NESW')
 
 		# time frame labels
 		time_label1 = tk.Label(self, text="min(s)")
@@ -45,21 +63,19 @@ class StartPage(tk.Frame):
 		time_label1.grid(row=0, column=2)
 		time_label2.grid(row=1, column=2)
 
-		# inputs
-		focus_input = tk.Entry(self, width=5)
-		break_input = tk.Entry(self, width=5)
-		focus_input.grid(row=0, column=1)
-		break_input.grid(row=1, column=1)
 
-		# button
-		next_button = tk.Button(self, text="Next", command=lambda: controller.show_frame(MainPage))
-		next_button.grid(row=2, columnspan=3, sticky='NESW')
+	
+		
 
 class MainPage(tk.Frame):
 
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
-		label = tk.Label(self, text="Main page")
+		self.current_session = "Focus"
+		self.current_time = (controller.focus_duration, 0) # (minutes, seconds)
+		self.focus_ct = 0
+		self.break_ct = 0
+		label = tk.Label(self, text=self.current_session)
 		label.pack()
 
 
